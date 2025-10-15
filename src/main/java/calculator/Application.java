73d2c8e2e1ecd7b6bd2643cf2e50fc423458738c
eunho <eunho1032@gmail.com>
2025-10-16 02:45:29 +0900

    feat: 기본 구분자 문자열 분리 및 합산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,23 @@
 package calculator;
-import static java.lang.Integer.parseInt;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         int result = 0;
+        int sum = 0;
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println("덧셈할 문자열을 입력해 주세요.\n" + input);
-        result = parseInt(input);
-        System.out.println("결과 : " + result);
+        String[] parts = input.split("[,:]");
+        for (String part : parts) {
+            if (!part.isEmpty())
+                sum += Integer.parseInt(part);
+        }
+        System.out.println("결과 : " + sum);
+    //TODO: 
+    /*
+    1. 빈 문자열 예외 처리
+    2. 커스텀 구분자 처리
+    3. 음수 문자 처리
+    */
     }
 }
