7d97f32d22eed203e7da9c189db635453a9e3525
eunho <eunho1032@gmail.com>
2025-10-17 01:52:09 +0900

    feat(run): 커스텀 구분자 구현 완료, 함수 분리 필요

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,42 +3,25 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
-    private final String DEFAULT_DELIMITER = "[,:]";
-
     public void run() {
-        String input = parseInput();
-        String delimiter = DEFAULT_DELIMITER;
-        String numbers = input;
-        int result = 0;
-
-        if (input.startsWith("//")) {
-            delimiter = String.valueOf(input.charAt(2));
-
-            String[] inputWithoutDelimiter = input.split("\\n", 2);
-            numbers = inputWithoutDelimiter[1];
-        }
-        String[] tokens = splitString(numbers, delimiter);
-        result = calculate(tokens);
-        System.out.println(result);
-    }
-
-    private String[] splitString(String numbers, String delimiter) {
-        return numbers.split(delimiter);
-    }
-
-    private String parseInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        if (input.isEmpty())
-            return "0";
-        return input;
-    }
-
-    private int calculate(String[] tokens) {
-        int sum = 0;
-        for (String token : tokens) {
-            sum += Integer.parseInt(token);
+        String delimiter = "[,:]";
+        String parseString = input;
+        int suffixIndex;
+        if (input.startsWith("//")) {
+            delimiter = String.valueOf(input.charAt(2));
+            suffixIndex = input.indexOf("\\n");
+            if (suffixIndex != -1) {
+                parseString = input.substring(suffixIndex + 2);
+            }
         }
-        return sum;
+
+        String[] numbers = parseString.split(delimiter);
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        System.out.println("결과 : " + sum);
     }
 }
