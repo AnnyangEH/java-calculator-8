7e48a1428c9974f65a313a3e6646ab108c7fcd5e
eunho <eunho1032@gmail.com>
2025-10-17 01:16:42 +0900

    feat(run): 새 함수들 추가 및 기본 작동 구현, 커스텀 구분자 처리 필요

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,33 +1,44 @@
 package calculator;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+    private final String DEFAULT_DELIMITER = "[,:]";
+
     public void run() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = parseString();
-        String[] numbers = splitString(input);
-        int result = calculate(numbers);
+        String input = parseInput();
+        String delimiter = DEFAULT_DELIMITER;
+        String numbers = input;
+        int result = 0;
+
+        if (input.startsWith("//")) {
+            delimiter = String.valueOf(input.charAt(2));
+
+            String[] inputWithoutDelimiter = input.split("\\n", 2);
+            numbers = inputWithoutDelimiter[1];
+        }
+        String[] tokens = splitString(numbers, delimiter);
+        result = calculate(tokens);
         System.out.println(result);
     }
 
-    private int calculate(String[] numbers) {
-        int sum = 0;
-
-        for (String number : numbers) {
-            if (!number.isEmpty())
-                sum += Integer.parseInt(number);
-        }
-        return sum;
+    private String[] splitString(String numbers, String delimiter) {
+        return numbers.split(delimiter);
     }
 
-    private String parseString() {
+    private String parseInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         if (input.isEmpty())
             return "0";
         return input;
     }
 
-    private String[] splitString(String input) {
-        return input.split("[,:]");
+    private int calculate(String[] tokens) {
+        int sum = 0;
+        for (String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
+        return sum;
     }
 }
