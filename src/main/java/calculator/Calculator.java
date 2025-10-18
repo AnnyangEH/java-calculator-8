818ce8d19211a1ca963e3fb7c9e72eee6705403f
eunho <eunho1032@gmail.com>
2025-10-19 01:01:10 +0900

    feat(validate): 예외처리 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,25 +3,80 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+    private static final String DEFAULT_DELIMITER = "[,:]";
     public void run() {
+        try {
+        String input = getUserInput();
+        validateInput(input);
+
+        String delimiter = parseDelimiter(input);
+        String numbers = parseNumber(input);
+        String[] tokens = numbers.split(delimiter);
+        validateTokens(tokens);
+
+        System.out.print("결과 : " + calculate(tokens));
+        } catch (IllegalArgumentException e) {
+            System.out.print("에러: " + e.getMessage());
+        }
+    }
+
+    private void validateInput(String input) {
+        if (input.startsWith("//")) {
+            if (input.length() < 5)
+                throw new IllegalArgumentException("커스텀 구분자 길이 오류");
+            int suffixIndex = input.indexOf("\\n");
+            if (suffixIndex != 3 || suffixIndex == -1)
+                throw new IllegalArgumentException("커스텀 구분자 형식 오류");
+        }
+    }
+
+    private void validateTokens(String[] tokens) {
+        long sum = 0;
+        long value = 0;
+        for (String token : tokens) {
+            if (token.isEmpty())
+                throw new IllegalArgumentException("123");
+            if (token.startsWith("-"))
+                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+            if (!token.matches("\\d+"))
+                throw new IllegalArgumentException("숫자 외 문자가 포함되었습니다.");
+            value = Long.parseLong(token);
+            sum += value;
+            // 오버플로우 방지
+            if (sum > Integer.MAX_VALUE)
+                throw new IllegalArgumentException("너무 큰 수 입니다.");
+        }
+    }
+
+
+    private String getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String delimiter = "[,:]";
-        String parseString = input;
-        int suffixIndex;
-        if (input.startsWith("//")) {
-            delimiter = String.valueOf(input.charAt(2));
-            suffixIndex = input.indexOf("\\n");
-            if (suffixIndex != -1) {
-                parseString = input.substring(suffixIndex + 2);
-            }
-        }
+        if (input.isEmpty())
+            input = "0";
+        return input;
+    }
 
-        String[] numbers = parseString.split(delimiter);
+    private String parseDelimiter(String input) {
+        String delimiter = DEFAULT_DELIMITER;
+        if (input.startsWith("//"))
+            delimiter = String.valueOf(input.charAt(2));
+        return delimiter;
+    }
+
+    private String parseNumber(String input) {
+        if (!input.startsWith("//"))
+            return input;
+        int suffixIndex = input.indexOf("\\n");
+        return input.substring(suffixIndex + 2);
+    }
+
+    private int calculate(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number);
         }
-        System.out.println("결과 : " + sum);
+        return sum;
     }
+
 }
